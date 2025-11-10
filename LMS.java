69da10f1ec202df484240128273f95630ed59afd
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-11-10 15:44:48 +0000

    Adding main menu

--- LMS.java
+++ LMS.java
@@ -4,5 +4,12 @@ public class LMS
     public static void main(String args[])
     {
         Scanner input = new Scanner(System.in);
+        System.out.println("\n=============================================");
+        System.out.println("               MAIN MENU");
+        System.out.println("=============================================");
+        System.out.println("1. Books Management");
+        System.out.println("2. Study Room Management");
+        System.out.println("3. Membership Management");
+        System.out.println("4. Exit System");
     }
 }
