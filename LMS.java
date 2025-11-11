cdbcccd0ad7b0944416a9e6ce506f0c4b2a20a9b
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-11-11 16:09:50 +0000

    Adding roomChoice variable to get user's consent in Study Room Management Menu

--- LMS.java
+++ LMS.java
@@ -68,12 +68,16 @@ public class LMS
         }
         if (choice == 2)
         {
+            int roomChoice;
             System.out.println("\n--- Study Room Management Menu ---");
             System.out.println("1. Show Available Study Rooms");
             System.out.println("2. Reserve a study room");
             System.out.println("3. Cancel Reservation");
             System.out.println("4. Check Reservation Status");
             System.out.println("5. Back to Main Menu");
+            System.out.println();
+            System.out.print("Enter choice: ");
+            roomChoice = input.nextInt();
         }
 
         if (choice == 3)
