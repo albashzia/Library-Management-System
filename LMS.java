2b3aa820e0ba33cea6ed55793bc58568d4c7d718
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-11-11 16:26:23 +0000

    Declaring MAX_BOOKS variable to hold the maximum number of books

--- LMS.java
+++ LMS.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class LMS
 {
+    static final int MAX_BOOKS = 100;
     public static void main(String args[])
     {
         Scanner input = new Scanner(System.in);
