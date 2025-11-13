18c1a71b9ee035ac52410b5eb4a08b49ff4b668f
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-11-13 15:36:12 +0000

    Removing manual display of Main Menu from the main body

--- LMS.java
+++ LMS.java
@@ -68,13 +68,7 @@ public class LMS
     public static void main(String args[])
     {
         Scanner input = new Scanner(System.in);
-        System.out.println("\n=============================================");
-        System.out.println("               MAIN MENU");
-        System.out.println("=============================================");
-        System.out.println("1. Books Management");
-        System.out.println("2. Study Room Management");
-        System.out.println("3. Membership Management");
-        System.out.println("4. Exit System");
+        
         System.out.println();
         
         System.out.print("Enter your choice: ");
